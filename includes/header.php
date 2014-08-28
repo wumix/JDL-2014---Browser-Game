05f090eaeaaede0b1f0c9aaa7dbfdd4aa960b543
stephano2013 <stephano2013@yahoo.com>
2014-08-28 11:52:37 +0300

    edited err

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 // File: header.php
 // Author: makingbrowsergames.com
 // Basic Tutorial
-
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 require('includes/smarty/Smarty.class.php');
 $smarty = new Smarty();
 
